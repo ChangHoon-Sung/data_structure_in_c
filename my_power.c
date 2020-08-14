5b73ac7c90174241e5d281bdfef62b61e3e8f03e
ChangHoon Sung <changhoon.dev@gmail.com>
2020-08-15 00:28:55 +0900

    Add iterative_power for compare

--- my_power.c
+++ my_power.c
@@ -19,11 +19,32 @@ double recursive_power(double x, int n)
     }
 }
 
+double iterative_power(double x, int n)
+{
+    double result = 1;
+    if (n == 0)
+        return result;
+    else
+    {
+        for (int i = 1; i <= n; i++)
+            result *= x;
+    }
+    return result;
+}
+
 int main()
 {
+    printf("Calculate 2^500, repeat 1 million times.\n");
+
     clock_t start = clock();
     for (int i = 0; i < 1000000; i++)
         recursive_power(2, 500);
     clock_t end = clock();
-    printf("recursive power takes %lf seconds", (double)(end - start) / CLOCKS_PER_SEC);
+    printf("recursive power takes %lf seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+    start = clock();
+    for (int i = 0; i < 1000000; i++)
+        iterative_power(2, 500);
+    end = clock();
+    printf("iterative power takes %lf seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
 }
